1ec1fec515fc6040e0b927b0e48db26f3d561b08
Contrabass26 <83555891+Contrabass26@users.noreply.github.com>
2024-08-06 23:38:52 +0100

    Red square

--- app/src/main/java/me/jsedwards/sudoku/MainActivity.kt
+++ app/src/main/java/me/jsedwards/sudoku/MainActivity.kt
@@ -3,13 +3,15 @@ package me.jsedwards.sudoku
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.SolidColor
 import me.jsedwards.sudoku.ui.theme.SudokuSolverTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +21,7 @@ class MainActivity : ComponentActivity() {
             SudokuSolverTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    SudokuCanvas()
                 }
             }
         }
@@ -27,17 +29,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SudokuSolverTheme {
-        Greeting("Android")
+fun SudokuCanvas(modifier: Modifier = Modifier) {
+    Canvas(modifier) {
+        val path = Path()
+        path.moveTo(0f, 0f)
+        path.lineTo(50f, 0f)
+        path.lineTo(50f, 50f)
+        path.lineTo(0f, 50f)
+        drawPath(path = path, brush = SolidColor(Color.Red))
     }
 }
