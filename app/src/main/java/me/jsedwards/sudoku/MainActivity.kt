907fcf89a8da039d0969a06c293dfff25de1860a
Contrabass26 <83555891+Contrabass26@users.noreply.github.com>
2024-08-07 14:01:17 +0100

    Grid and selection

--- app/src/main/java/me/jsedwards/sudoku/MainActivity.kt
+++ app/src/main/java/me/jsedwards/sudoku/MainActivity.kt
@@ -4,24 +4,49 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Fill
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.TextUnitType
 import me.jsedwards.sudoku.ui.theme.SudokuSolverTheme
+import kotlin.math.floor
+
+const val MARGIN = 20f
+
+private val possibilities = mutableMapOf<Pair<Int, Int>, MutableList<Int>>()
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             SudokuSolverTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    SudokuCanvas()
+                    Column {
+                        SudokuCanvas()
+                        Button({ println("Hello from button") }) {
+                            Text("Cheese")
+                        }
+                    }
                 }
             }
         }
@@ -30,7 +55,47 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SudokuCanvas(modifier: Modifier = Modifier) {
-    Canvas(modifier) {
-        drawRect(color = Color.Green, size = Size(300f, 300f), topLeft = Offset(size.width / 2f - 150, size.height / 2f - 150))
+    val textMeasurer = rememberTextMeasurer(9)
+    var selectedSquareX by remember { mutableIntStateOf(0) }
+    var selectedSquareY by remember { mutableIntStateOf(0) }
+    BoxWithConstraints {
+        Canvas(modifier.size(maxWidth, maxWidth).pointerInput(Unit) {
+            detectTapGestures { offset ->
+                selectedSquareX = floor((offset.x - MARGIN) / (size.width - MARGIN) * 9).toInt()
+                selectedSquareY = floor((offset.y - MARGIN) / (size.width - MARGIN) * 9).toInt()
+            }
+        }) {
+            println("Drawing canvas")
+            val gridSize = size.width - MARGIN * 2
+            val squareSize = gridSize / 9
+            val gridEnd = size.width - MARGIN
+            // Selected square
+            drawRect(Color(250, 218, 74), Offset(MARGIN + selectedSquareX * squareSize, MARGIN + selectedSquareY * squareSize), Size(squareSize, squareSize), style = Fill)
+            // Main square
+            drawRect(Color.Black, Offset(MARGIN, MARGIN), Size(gridSize, gridSize), style = Stroke(width = 20f))
+            // Other lines
+            for (i in 1..8) {
+                val strokeWidth = if (i % 3 == 0) 10f else 5f
+                val offset = MARGIN + i * squareSize
+                drawLine(Color.Black, Offset(offset, MARGIN), Offset(offset, gridEnd), strokeWidth)
+                drawLine(Color.Black, Offset(MARGIN, offset), Offset(gridEnd, offset), strokeWidth)
+            }
+            // Numbers
+            possibilities.filterValues { it.size == 1 }.forEach { (x, y), (value) ->
+                val layoutResult = textMeasurer.measure(
+                    value.toString(),
+                    style = TextStyle(
+                        fontSize = TextUnit(36f, TextUnitType.Sp),
+                        fontFamily = FontFamily(Font(R.font.karnak_regular, FontWeight.Normal))
+                    )
+                )
+                drawText(
+                    layoutResult, Color.Black, topLeft = Offset(
+                        (MARGIN + (x + 0.5) * squareSize - layoutResult.size.width / 2.0).toFloat(),
+                        (MARGIN + (y + 0.5) * squareSize - layoutResult.size.height / 2.0).toFloat()
+                    )
+                )
+            }
+        }
     }
 }
